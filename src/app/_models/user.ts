a5ad2e7b3ca310ce3d125b212241c589e77dad30
smuralee <suraj@surajmuraleedharan.com>
2021-01-01 23:34:46 +0000

    WIP: models and service update

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,6 +1,10 @@
+import {Order} from './order';
+
 export class User {
   firstName?: string;
   middleName?: string;
   lastName?: string;
+  name?: string;
   age?: number;
+  orders?: Order[];
 }
